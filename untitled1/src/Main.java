6e6b179129c6a3c13c93ce190cb884f8f0a4ee57
Do-yoon Kim <100352835+doyooning@users.noreply.github.com>
2025-08-27 19:01:43 +0900

    250827 Programmers
    250827 Programmers

---
+++ untitled1/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    static Solution sol = new Solution();
+    public static void main(String[] args) {
+        String[] words = {"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
+        System.out.println(sol.solution(5, words));
+    }
+}
